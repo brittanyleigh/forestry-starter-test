c36ac7371bc85daea2c64f616fe599c26c5db64b
Brittany Isenberg <brittany.isenberg@gmail.com>
2020-05-14 17:28:04 -0400

    create page from json data

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,20 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import data from "../../content/homepage.json"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>{data.title}</h1>
+    {data.content.map((item, index) => {
+      return (
+        <div key={`content_item_${index}`}>
+          <h2>{item.heading}</h2>
+          <p>{item.body}</p>
+        </div>
+      )
+    })}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
